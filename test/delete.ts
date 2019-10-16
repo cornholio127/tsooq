8b154b435aa115448e4e8bc1df233a03bd3f25fb
Mathias Haessig <cornholio127@live.com>
2019-10-17 00:58:49 +0200

    Add test

--- test/delete.ts
+++ test/delete.ts
@@ -1,6 +1,7 @@
 import 'mocha';
 import chai from 'chai';
-import { Tables, Person, useCreate } from './setup';
+import { Tables, Person, useCreate, Address } from './setup';
+import { select } from '../src/impl';
 
 describe.only('delete', () => {
   describe('simple delete', () => {
@@ -31,6 +32,28 @@ describe.only('delete', () => {
           chai.assert.equal(
             stub.getCall(0).args[0],
             'DELETE FROM person WHERE person.id = $1',
+          );
+          done();
+        })
+        .catch(err => done(err));
+    });
+  });
+
+  describe('delete with subquery', () => {
+    it('should render correct sql', done => {
+      const [create, _, stub] = useCreate();
+      create
+        .deleteFrom(Tables.PERSON)
+        .where(Person.ADDRESS_ID.in(
+          select(Address.ID)
+            .from(Tables.ADDRESS)
+            .where(Address.CITY.eq('London'))
+        ))
+        .execute()
+        .then(() => {
+          chai.assert.equal(
+            stub.getCall(0).args[0],
+            'DELETE FROM person WHERE person.address_id IN (SELECT address.id FROM address WHERE address.city = $1)',
           );
           done();
         })
